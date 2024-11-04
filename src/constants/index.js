8d6f7ebecd21810982ea9b319e6e2b7b44fa1f43
Abhijit Deka <deka.abhijit1204@gmail.com>
2024-11-04 12:20:55 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -245,7 +245,7 @@ const projects = [
     ],
     image: second_project,
     source_code_link:
-      "https://www.figma.com/proto/cNUFoZGpU7Upel97IbBwQl?node-id=0-9",
+      "https://www.figma.com/proto/qy4qLPP6sEsguTwqYQdxdA/3D-Interactive-Planet-Animation?page-id=0%3A1&node-id=1-473&node-type=canvas&viewport=91%2C-2260%2C0.85&t=QiDaoY4do6JbWllm-1&scaling=contain&content-scaling=fixed&starting-point-node-id=0%3A9",
   },
   {
     name: "3D Modelling (UX)",
